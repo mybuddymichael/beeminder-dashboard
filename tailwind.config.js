0fb95b6ba759279ff55a6095a09b9185cd081abb
Michael Hanson <186724+mybuddymichael@users.noreply.github.com>
2024-02-07 21:29:58 -0800

    Add/update tailwind/prettier files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		extend: {}
+	},
+	plugins: [],
+	corePlugins: {
+		preflight: false
+	}
+};
